124df0094e132e35ba892b49f24b0733c185efb1
sandeepchawla <sandeep.chawla322@webkul.in>
2025-10-03 12:41:06 +0530

    Refactor email sending logic for Purchase Orders and RFQs to display detailed notification messages

--- plugins/webkul/purchases/resources/lang/en/filament/admin/clusters/orders/resources/order/actions/send-email.php
+++ plugins/webkul/purchases/resources/lang/en/filament/admin/clusters/orders/resources/order/actions/send-email.php
@@ -3,6 +3,8 @@
 return [
     'label'        => 'Send By Email',
     'resend-label' => 'Re-Send By Email',
+    'quotation'    => 'quotation',
+    'quotations'   => 'quotations',
 
     'form' => [
         'fields' => [
@@ -12,11 +14,31 @@ return [
         ],
     ],
 
-    'action' => [
+    'actions' => [
         'notification' => [
-            'success' => [
-                'title' => 'Email sent',
-                'body'  => 'The email has been sent successfully.',
+            'email' => [
+                'no_recipients' => [
+                    'title' => 'No Recipients Selected',
+                    'body'  => 'Please select at least one vendor to send quotations to.',
+                ],
+
+                'all_success' => [
+                    'title' => 'Quotations Sent!',
+                    'body'  => 'Your :plural have been successfully delivered to: :recipients',
+                ],
+
+                'all_failed' => [
+                    'title' => 'Unable to Send Quotations',
+                    'body'  => 'We encountered issues sending your quotations: :failures',
+                ],
+
+                'partial_success' => [
+                    'title'       => 'Some Quotations Sent',
+                    'sent_part'   => 'Successfully delivered to: :recipients',
+                    'failed_part' => 'Could not deliver to: :failures',
+                ],
+
+                'failure_item' => ':partner (:reason)',
             ],
         ],
     ],
