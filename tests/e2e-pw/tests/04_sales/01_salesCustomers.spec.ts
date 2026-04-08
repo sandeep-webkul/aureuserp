1cc47ad7c9f4d502ef1c01e3f4daa7f3ed59bf1e
harshqa-webkul <harshgupta.qa322@webkul.in>
2026-04-08 17:11:00 +0530

    fix: files naming

--- tests/e2e-pw/tests/04_sales/01_salesCustomers.spec.ts
+++ tests/e2e-pw/tests/04_sales/01_salesCustomers.spec.ts
@@ -1,5 +1,5 @@
 import { test } from "../../setup";
-import { SalesFlowPage } from "../../pages/05_salesFlow";
+import { SalesFlowPage } from "../../pages/04_salesFlow";
 
 test.describe("Sales Customers E2E", () => {
     test.beforeAll(async ({ adminPage }) => {
