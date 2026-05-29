78e66c8ee5701fd9d7ec513c3765903d54ae14c0
XoloCodigo <ricardo96.mgz@outlook.com>
2026-05-28 22:29:03 -0600

    feat(products): add Spanish (es) translations for models

---
+++ plugins/webkul/products/resources/lang/es/models/product.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'title'          => 'Producto',
+
+    'log-attributes' => [
+        'type'                => 'Tipo',
+        'name'                => 'Nombre',
+        'service_tracking'    => 'Seguimiento de servicio',
+        'reference'           => 'Referencia',
+        'barcode'             => 'Código de barras',
+        'price'               => 'Precio',
+        'cost'                => 'Costo',
+        'volume'              => 'Volumen',
+        'weight'              => 'Peso',
+        'description'         => 'Descripción',
+        'description_purchase'=> 'Descripción (Compra)',
+        'description_sale'    => 'Descripción (Venta)',
+        'enable_sales'        => 'Activar ventas',
+        'enable_purchase'     => 'Activar compras',
+        'is_favorite'         => 'Es favorito',
+        'is_configurable'     => 'Es configurable',
+        'parent'              => 'Superior',
+        'category'            => 'Categoría',
+        'company'             => 'Empresa',
+        'creator'             => 'Creador',
+    ],
+];
