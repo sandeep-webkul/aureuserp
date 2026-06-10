e919abb8d653170b05ddd9980b9552ff0233057c
Jitendra <jitendra@webkul.in>
2026-06-10 12:07:25 +0530

    Made next transder as nested as well

--- plugins/webkul/sales/src/Filament/Clusters/Orders/Resources/QuotationDeliveryResource/Pages/ViewDelivery.php
+++ plugins/webkul/sales/src/Filament/Clusters/Orders/Resources/QuotationDeliveryResource/Pages/ViewDelivery.php
@@ -3,9 +3,12 @@
 namespace Webkul\Sale\Filament\Clusters\Orders\Resources\QuotationDeliveryResource\Pages;
 
 use Webkul\Inventory\Filament\Clusters\Operations\Resources\DeliveryResource\Pages\ViewDelivery as BaseViewDelivery;
+use Webkul\Sale\Filament\Clusters\Orders\Resources\Concerns\ReplacesNextTransferAction;
 use Webkul\Sale\Filament\Clusters\Orders\Resources\QuotationDeliveryResource;
 
 class ViewDelivery extends BaseViewDelivery
 {
+    use ReplacesNextTransferAction;
+
     protected static string $resource = QuotationDeliveryResource::class;
 }
