2080533404d701b61359d37cf35c57d0c602047b
Jitendra <jitendra@webkul.in>
2026-06-10 11:32:55 +0530

    Fixed quotation messaging issues

--- plugins/webkul/sales/src/Filament/Clusters/Orders/Resources/OrderResource.php
+++ plugins/webkul/sales/src/Filament/Clusters/Orders/Resources/OrderResource.php
@@ -7,6 +7,7 @@ use Filament\Resources\Pages\Page;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Webkul\Sale\Enums\OrderState;
+use Webkul\Sale\Models\Order;
 use Webkul\Sale\Filament\Clusters\Orders\Resources\OrderResource\Pages\CreateOrder;
 use Webkul\Sale\Filament\Clusters\Orders\Resources\OrderResource\Pages\EditOrder;
 use Webkul\Sale\Filament\Clusters\Orders\Resources\OrderResource\Pages\ListOrders;
@@ -18,6 +19,8 @@ use Webkul\Security\Traits\HasResourcePermissionQuery;
 class OrderResource extends QuotationResource
 {
     use HasResourcePermissionQuery;
+
+    protected static ?string $model = Order::class;
 
     protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-shopping-bag';
 
